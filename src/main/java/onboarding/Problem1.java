99d3c3748584363c9fab2ccda857fdfbaa6a38d1
dhk767 <dhk767@gmail.com>
2022-10-26 23:12:53 +0900

    1. Problem1 구현 완료

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,69 @@
 package onboarding;
 
 import java.util.List;
+import java.util.Map;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if (pobi.get(1) - pobi.get(0) != 1 || crong.get(1) - crong.get(0) != 1){
+            return -1;
+        }
+        try {
+            int pobiPlus = 0;
+            int pobiMulti = 1;
+            int pobiFinalPoint = 0;
+
+            for (int i = 0; i < pobi.size(); i++) {
+                String temp = pobi.get(i).toString();
+                for (int j = 0; j < temp.length(); j++) {
+                    pobiPlus += (temp.charAt(j) - '0');
+                    pobiMulti = pobiMulti * (temp.charAt(j) - '0');
+
+                }
+                if (pobiPlus > pobiMulti) {
+                    pobiFinalPoint = pobiFinalPoint > pobiPlus? pobiFinalPoint : pobiPlus;
+                } else {
+                    pobiFinalPoint = pobiFinalPoint > pobiMulti? pobiFinalPoint : pobiMulti;
+                }
+                pobiPlus = 0;
+                pobiMulti = 1;
+
+            }
+
+            int crongPlus = 0;
+            int crongMulti = 1;
+            int crongFinalPoint = 0;
+
+            for (int i = 0; i < crong.size(); i++) {
+                String temp = crong.get(i).toString();
+                for (int j = 0; j < temp.length(); j++) {
+                    crongPlus += (temp.charAt(j) - '0');
+                    crongMulti = crongMulti * (temp.charAt(j) - '0');
+
+                }
+                if (crongPlus > crongMulti) {
+                    crongFinalPoint = crongFinalPoint > crongPlus? crongFinalPoint : crongPlus;
+                } else {
+                    crongFinalPoint = crongFinalPoint > crongMulti ? crongFinalPoint : crongMulti;
+                }
+                crongPlus = 0;
+                crongMulti = 1;
+            }
+
+            if (crongFinalPoint > pobiFinalPoint) {
+                answer = 2;
+            } else if (crongFinalPoint < pobiFinalPoint) {
+                answer = 1;
+            } else {
+                answer = 0;
+            }
+
+        }catch (Exception e){
+            return -1;
+        }
+
         return answer;
     }
 }
