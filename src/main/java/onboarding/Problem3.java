d5a6425aa58c207ab697795e1a36b3c851957328
dhk767 <dhk767@gmail.com>
2022-10-27 00:15:34 +0900

    3. 3번 문제 구현 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,23 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
-        return answer;
+
+        return calc(number);
+
     }
+
+    public static int calc(int num) {
+
+        int result = 0;
+
+        for (int i = 1; i <= num; i++){
+            String temp = Integer.toString(i);
+            result += temp.chars()
+                    .filter(c -> c == '3' || c == '6' || c =='9')
+                    .count();
+        }
+
+        return result;
+    }
+
 }
