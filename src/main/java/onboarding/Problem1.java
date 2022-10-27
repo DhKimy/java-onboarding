006185859416900757b6c47f6d18fe5afe5d9ed9
dhk767 <dhk767@gmail.com>
2022-10-27 17:08:55 +0900

    refac : Problem1 예외처리 방식 변경

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -10,58 +10,58 @@ class Problem1 {
         if (pobi.get(1) - pobi.get(0) != 1 || crong.get(1) - crong.get(0) != 1){
             return -1;
         }
-        try {
-            int pobiPlus = 0;
-            int pobiMulti = 1;
-            int pobiFinalPoint = 0;
 
-            for (int i = 0; i < pobi.size(); i++) {
-                String temp = pobi.get(i).toString();
-                for (int j = 0; j < temp.length(); j++) {
-                    pobiPlus += (temp.charAt(j) - '0');
-                    pobiMulti *= (temp.charAt(j) - '0');
+        // 예외
+        /**
+         * 페이지 차이가 1이 아닐 경우
+         * 페이지가 음수 또는 400을 초과하여 입력될 경우
+         */
 
-                }
-                if (pobiPlus > pobiMulti) {
-                    pobiFinalPoint = pobiFinalPoint > pobiPlus? pobiFinalPoint : pobiPlus;
-                } else {
-                    pobiFinalPoint = pobiFinalPoint > pobiMulti? pobiFinalPoint : pobiMulti;
-                }
-                pobiPlus = 0;
-                pobiMulti = 1;
+        int pobiPlus = 0;
+        int pobiMulti = 1;
+        int pobiFinalPoint = 0;
+
+        for (int i = 0; i < pobi.size(); i++) {
+            String temp = pobi.get(i).toString();
+            for (int j = 0; j < temp.length(); j++) {
+                pobiPlus += (temp.charAt(j) - '0');
+                pobiMulti *= (temp.charAt(j) - '0');
 
             }
-
-            int crongPlus = 0;
-            int crongMulti = 1;
-            int crongFinalPoint = 0;
-
-            for (int i = 0; i < crong.size(); i++) {
-                String temp = crong.get(i).toString();
-                for (int j = 0; j < temp.length(); j++) {
-                    crongPlus += (temp.charAt(j) - '0');
-                    crongMulti = crongMulti * (temp.charAt(j) - '0');
-
-                }
-                if (crongPlus > crongMulti) {
-                    crongFinalPoint = crongFinalPoint > crongPlus? crongFinalPoint : crongPlus;
-                } else {
-                    crongFinalPoint = crongFinalPoint > crongMulti ? crongFinalPoint : crongMulti;
-                }
-                crongPlus = 0;
-                crongMulti = 1;
-            }
-
-            if (crongFinalPoint > pobiFinalPoint) {
-                answer = 2;
-            } else if (crongFinalPoint < pobiFinalPoint) {
-                answer = 1;
+            if (pobiPlus > pobiMulti) {
+                pobiFinalPoint = pobiFinalPoint > pobiPlus? pobiFinalPoint : pobiPlus;
             } else {
-                answer = 0;
+                pobiFinalPoint = pobiFinalPoint > pobiMulti? pobiFinalPoint : pobiMulti;
             }
+            pobiPlus = 0;
+            pobiMulti = 1;
+        }
 
-        }catch (Exception e){
-            return -1;
+        int crongPlus = 0;
+        int crongMulti = 1;
+        int crongFinalPoint = 0;
+
+        for (int i = 0; i < crong.size(); i++) {
+            String temp = crong.get(i).toString();
+            for (int j = 0; j < temp.length(); j++) {
+                crongPlus += (temp.charAt(j) - '0');
+                crongMulti = crongMulti * (temp.charAt(j) - '0');
+            }
+            if (crongPlus > crongMulti) {
+                crongFinalPoint = crongFinalPoint > crongPlus? crongFinalPoint : crongPlus;
+            } else {
+                crongFinalPoint = crongFinalPoint > crongMulti ? crongFinalPoint : crongMulti;
+            }
+            crongPlus = 0;
+            crongMulti = 1;
+        }
+
+        if (crongFinalPoint > pobiFinalPoint) {
+            answer = 2;
+        } else if (crongFinalPoint < pobiFinalPoint) {
+            answer = 1;
+        } else {
+            answer = 0;
         }
 
         return answer;
